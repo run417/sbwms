0dbeb00cf3bf9f189ca961733c52376829d48d06
Vinura <vinuraeu@gmail.com>
2019-08-11 20:20:24 +0530

    Make new login view
    Also included its controller

---
+++ resources/views/login.view.php
@@ -0,0 +1,73 @@
+<?php
+    require_once(COMMON_VIEWS . 'header.php');
+?>
+<style>
+    .image {
+    min-height: 100vh;
+    }
+
+    .login {
+        min-height: 100vh;
+        position: relative;
+    }
+
+    .login::before {
+        content: "";
+        top: 0; left: 0;
+        width: 100%; height: 100%;
+        position: absolute;
+        background-image: url('/sbwms/public/assets/img/car-part-pattern.png');
+        background-size: cover;
+        filter: opacity(17%);
+        /* filter: contrast(%); */
+        /* filter: invert(50%); */
+        /* filter: brightness(1%); */
+    }
+     h3 {
+         font-family: Comfortaa;
+         /* font-weight: 600; */
+     }
+
+    /* .bg-image {
+    background-image: url('https://res.cloudinary.com/mhmd/image/upload/v1555917661/art-colorful-contemporary-2047905_dxtao7.jpg');
+    background-size: cover;
+    background-position: center center;
+    } */
+
+
+</style>
+<body>
+    <div class="container-fluid">
+        <div class="row no-gutter">
+
+            <!-- The Image Half -->
+            <div class="col-md-7 d-none d-md-flex bg-image"></div>
+
+            <!-- The Login Content Half -->
+            <div class="col-md-5 bg-light login d-flex align-items-center py-5">
+                <!-- <div class=""> -->
+                    <!-- Content -->
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-10 col-xl-7 mx-auto">
+                                <h3 class="mb-5 text-center">SBWMS Login</h3>
+                                <form style="width: 250px" class="mx-auto">
+                                    <div class="form-group mb-3">
+                                        <input type="text" placeholder="Username" class="form-control px-4 py-1 shadow-sm">
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <input type="password" placeholder="Password" class="form-control px-4 py-1 shadow-sm">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-block text-uppercase mb-2 shadow-sm">Log in</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                <!-- </div> -->
+            </div>
+
+        </div> <!-- </row.no-gutter> -->
+    </div> <!-- </container-fluid> -->
+
+    <?php require_once(COMMON_VIEWS . 'footer.php'); ?>
+</body>
