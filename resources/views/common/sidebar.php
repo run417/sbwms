3a87b8cb93abf4dbefcc3857e53bcb447ed0e8bb
Vinura <vinuraeu@gmail.com>
2019-08-14 19:44:05 +0530

    Update sidebar menu links

--- resources/views/common/sidebar.php
+++ resources/views/common/sidebar.php
@@ -14,68 +14,58 @@
         </li> -->
         <!-- <li class="active"> -->
         <li>
-            <a href="<?php echo url_for('/public'); ?>"><i class="fas fa-chart-line"></i>Dashboard</a>
+            <a href="<?php echo url_for('/'); ?>" id="dashboard_menu"><i class="fa-fw fas fa-chart-line"></i>Dashboard</a>
+        </li>
+        
+        <li>
+            <a href="<?php echo url_for('/booking/list.php') ?>" id="booking_menu"><i class="fa-fw fas fa-book"></i>Booking</a>
+        </li>
+       <!-- <ul>
+            <li><a href="#">List</a></li>
+            <li><a href="#">Walk-in</a></li>
+            <li><a href="#">Verify Online Booking</a></li>
+        </ul> -->
+    
+        <li>
+            <a href="<?php echo url_for('/service'); ?>" id="#service_menu"><i class="fa-fw fas fa-car"></i>Service</a>
+        </li>
+        <!-- <ul class="collapse list-unstyled" id="service_menu">
+            <li><a href="#"><i class="fas fa-tasks"></i>Service Order</a></li>
+            <li><a href="#">Service Schedule</a></li>
+            <li><a href="/public/views/service/service_type/index.php')">Service Types</a></li>
+            <li><a href="#">Bays</a></li>
+            <li><a href="#">Ongoing Services</a></li>
+        </ul> -->
+    
+        <li>
+            <a href="<?php echo url_for('/inventory'); ?>" id="inventory_menu"><i class="fa-fw fas fa-archive"></i>Inventory</a>
         </li>
         <li>
-            <a href="#bookingSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-car-crash"></i>Booking</a>
-            <ul class="collapse list-unstyled" id="bookingSubmenu">
-                <li><a href="<?php echo url_for('/public/views/booking/list.php');?>">List</a></li>
-                <li><a href="<?php echo url_for('/public/views/service/service_type/index.php'); ?>">Walk-in</a></li>
-                <li><a href="<?php echo url_for('/public/views/booking/verify_online_booking.php'); ?>">Verify Online Booking</a></li>
-            </ul>
+            <a href="<?php echo url_for('/sale/list.php'); ?>"><i class="fa-fw fas fa-cash-register"></i>Sales</a>
         </li>
-        <li>
-            <a href="#"><i class="fas fa-tasks"></i>Service Order</a>
-        </li>
-        <li>
-            <a href="#serviceSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-car-crash"></i>Services</a>
-            <ul class="collapse list-unstyled" id="serviceSubmenu">
-                <li><a href="#">Service Schedule</a></li>
-                <li><a href="<?php echo url_for('/public/views/service/service_type/index.php'); ?>">Service Types</a></li>
-                <li><a href="#">Bays</a></li>
-                <li><a href="#">Ongoing Services</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="#inventorySubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-archive"></i>Inventory</a>
-            <ul class="collapse list-unstyled" id="inventorySubmenu">
-                <li><a href="<?php echo url_for('/public/views/inventory/product/index.php'); ?>">Products</a></li>
-                <li><a href="<?php echo url_for('/public/views/inventory/category/index.php'); ?>">Categories</a></li>
-                <li><a href="#">Purchase Orders</a></li>
-                <li><a href="<?php echo url_for('/public/views/inventory/supplier/index.php'); ?>">Suppliers</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="#saleSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-store"></i>Sales</a>
-            <ul class="collapse list-unstyled" id="saleSubmenu">
-                <li><a href="<?php echo url_for('/public/views/sale/order/newOrder.php'); ?>">Orders</a></li>
+            <!-- <ul class="collapse list-unstyled" id="saleSubmenu">
+                <li><a href="#">Orders</a></li>
                 <li><a href="#">Returns</a></li>
                 <li><a href="#">Order History</a></li>
-            </ul>
+            </ul> -->
+        <li>
+            <a href="<?php echo url_for('/customer/list.php'); ?>" id="customer_menu"><i class="fa-fw fas fa-user"></i>Customer</a>
         </li>
         <li>
-            <a href="#customer" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-user-friends"></i>Customer</a>
-            <ul class="collapse list-unstyled" id="customer">
-                <li><a href="<?php echo url_for('/public/views/customer/index.php'); ?>"></i>Customers</a></li>
-                <li><a href="<?php echo url_for('/public/views/customer/onlineProfile/index.php'); ?>"></i>Online Profiles</a></li>
-            </ul>
-            
+            <a href="<?php echo url_for('/employee/list.php'); ?>" id="employee_menu"><i class="fa-fw fas fa-id-badge"></i>Employee</a>
         </li>
-            <li>
-                <a href="<?php echo url_for('/public/views/employee/index.php'); ?>"><i class="fas fa-id-badge"></i>Employees</a>
-            </li>
         <li>
-            <a href="#comSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-comment"></i>Communication</a>
-            <ul class="collapse list-unstyled" id="comSubmenu">
+            <a href="<?php echo url_for('/communication/'); ?>"><i class="fa-fw fas fa-comment"></i>Communication</a>
+            <!-- <ul class="collapse list-unstyled" id="comSubmenu">
                 <li><a href="#">Customer Queries</a></li>
                 <li><a href="#">Notifications</a></li>
-            </ul>
+            </ul> -->
         </li>
-            <li>
-                <a href="#system" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-cog"></i>System</a>
-                <ul class="collapse list-unstyled" id="system">
-                    <li><a href="<?php echo url_for('/public/views/system-administration/user/index.php'); ?>">Users</a></li>
-                </ul>
-            </li>
+        <li>
+            <a href="#system"><i class="fa-fw fas fa-cog"></i>System</a>
+        </li>
+            <!-- <ul class="collapse list-unstyled" id="system">
+                <li><a href="/public/views/system-administration/user/index.php">Users</a></li>
+            </ul> -->
     </ul>
 </nav>
