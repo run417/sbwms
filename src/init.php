cd7f4547fa4ec303434033594d197059801f8916
Vinura <vinuraeu@gmail.com>
2019-08-18 11:58:52 +0530

    Require composer autoload.php

--- src/init.php
+++ src/init.php
@@ -2,6 +2,7 @@
 define("WWW_ROOT", "/sbwms/public");
 define("PUBLIC_PATH", getProjectPath() . '/public');
 define("COMMON_VIEWS", getProjectPath() . '/resources/views/common/');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 /**
  * Get the project path.
