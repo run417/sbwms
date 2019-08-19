6e6470a5847ccfdd0b6a2c9c74059bbd4f7a7ddc
Vinura <vinuraeu@gmail.com>
2019-08-19 13:39:33 +0530

    Add VIEWS constant

--- src/init.php
+++ src/init.php
@@ -3,4 +3,5 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 require_once('helpers.php');
 define("WWW_ROOT", "/sbwms/public");
 define("PUBLIC_PATH", getProjectPath() . '/public');
+define("VIEWS", getProjectPath() . '/resources/views/');
 define("COMMON_VIEWS", getProjectPath() . '/resources/views/common/');
