70991381dffc264854d3e706d0476c6f6250eb3e
Vinura <vinuraeu@gmail.com>
2019-08-19 13:18:34 +0530

    Link separated files in init.php

--- src/init.php
+++ src/init.php
@@ -1,6 +1,6 @@
 <?php
+require_once(__DIR__ . '/../vendor/autoload.php');
+require_once('helpers.php');
 define("WWW_ROOT", "/sbwms/public");
 define("PUBLIC_PATH", getProjectPath() . '/public');
 define("COMMON_VIEWS", getProjectPath() . '/resources/views/common/');
-require_once(__DIR__ . '/../vendor/autoload.php');
-
