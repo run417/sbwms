179a206f44cb492f1fb318ea25aa49002f427614
Vinura <vinuraeu@gmail.com>
2019-08-21 20:19:27 +0530

    Update file paths

--- src/init.php
+++ src/init.php
@@ -3,8 +3,8 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 require_once('helpers.php');
 define("WWW_ROOT", "/sbwms/public");
 define("PUBLIC_PATH", getProjectPath() . '/public');
-define("VIEWS", getProjectPath() . '/resources/views/');
-define("COMMON_VIEWS", getProjectPath() . '/resources/views/common/');
+define("VIEWS", getProjectPath() . '/src/views/');
+define("COMMON_VIEWS", getProjectPath() . '/src/views/common/');
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
