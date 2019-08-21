8e04e9a6608f07bb205062646f052e19e417ac94
Vinura <vinuraeu@gmail.com>
2019-08-21 12:59:39 +0530

    Checkout init.php from dev branch

--- src/init.php
+++ src/init.php
@@ -5,3 +5,10 @@ define("WWW_ROOT", "/sbwms/public");
 define("PUBLIC_PATH", getProjectPath() . '/public');
 define("VIEWS", getProjectPath() . '/resources/views/');
 define("COMMON_VIEWS", getProjectPath() . '/resources/views/common/');
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$response = new Response();
+$response->prepare($request);
