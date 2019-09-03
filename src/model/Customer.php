6e79f81c90db2f665cfa3bc772f761682b5a4bfb
Vinura <vinuraeu@gmail.com>
2019-09-03 10:41:57 +0530

    Add more properties to Customer class

--- src/model/Customer.php
+++ src/model/Customer.php
@@ -4,17 +4,21 @@ namespace sbwms;
 
 class Customer {
     private $customerId;
+    private $title;
     private $firstName;
     private $lastName;
     private $telephone;
     private $email;
+    private $regDate;
 
     public function __construct($args = []) {
         $this->customerId = $args['customerId'] ?? null;
-        $this->lastName = $args['lastName'] ?? null;
+        $this->title = $args['title'] ?? null;
         $this->firstName = $args['firstName'] ?? null;
+        $this->lastName = $args['lastName'] ?? null;
         $this->telephone = $args['telephone'] ?? null;
         $this->email = $args['email'] ?? null;
+        $this->regDate = $args['regDate'] ?? null;
     }
 
     /**
@@ -55,5 +59,21 @@ class Customer {
     public function getEmail()
     {
         return $this->email;
+    }
+
+    /**
+     * Get the value of regDate
+     */ 
+    public function getRegDate()
+    {
+        return $this->regDate;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
     }
 }
