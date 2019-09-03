69c23f9c9bafb51f46f502ca87accadc878cd5d8
Vinura <vinuraeu@gmail.com>
2019-09-03 21:37:26 +0530

    Remove PUBLIC_PATH constant

--- src/init.php
+++ src/init.php
@@ -2,7 +2,6 @@
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once('helpers.php');
 define("WWW_ROOT", "/sbwms/public");
-define("PUBLIC_PATH", projectRoot() . '/public');
 define("VIEWS", projectRoot() . '/src/views/');
 define("COMMON_VIEWS", projectRoot() . '/src/views/common/');
 
