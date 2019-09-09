a74d8fbbb54acc3ed3a839a4c549014c32bc0106
Vinura <vinuraeu@gmail.com>
2019-09-09 13:52:11 +0530

    Add new customer handler

--- src/handlers/customer/new.php
+++ src/handlers/customer/new.php
@@ -1,26 +1,38 @@
 <?php
-$postcount = ($request->request->count()); // $_POST Variables
-if ($postcount !== 0) {
-    /* Customer Details */
-    $firstname = $request->request->get('customer_first_name');
-    $lastName = $request->request->get('customer_last_name');
-    $telephone = $request->request->get('customer_telephone');
-    $email = $request->request->get('customer_email');
+use sbwms\Customer;
+use sbwms\CustomerMapper;
+use sbwms\CustomerRepository;
 
-    /* Vehicle Details */
-    $make = $request->request->get('vehicle_make');
-    $model = $request->request->get('vehicle_model');
-    $year = $request->request->get('vehicle_year');
-    $registrationNo = $request->request->get('vehicle_registration_no');
-    $vinNo = $request->request->get('vehicle_id_no');
+if ($request->getMethod() === 'POST') {
+
+    /** @var array */
+    $formData = $request->request->getIterator()->getArrayCopy();
+
+    /** @var CustomerMapper */
+    $customerMapper = new CustomerMapper($pdoAdapter);
+
+    /** @var Customer */
+    $customer = $customerMapper->create($formData);
+
+    /** @var CustomerRepository */
+    $customerRepository = new CustomerRepository($customerMapper);
+
+    /** @var true|null */
+    $result = $customerRepository->save($customer);
+    
     
     /* 
         0 = success
         1 = failure (expect error list)
     */
 
-    $result = 0;
-    echo $result; // buffered
+    /** @var int */
+    $successStatus = 1;
+
+    if ($result === true) { $successStatus = 0; }
+
+    echo $successStatus; // buffered
     return;
 }
 require_once VIEWS . 'customer/createCustomer.view.php';
+// require_once VIEWS . 'customer/new.customer.partial.view.php';
