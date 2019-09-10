290c284da0cf2ca04090583862ae6aa64ff3f727
Vinura <vinuraeu@gmail.com>
2019-09-10 22:32:58 +0530

    Add new employee view and handlers

--- src/handlers/employee/new.php
+++ src/handlers/employee/new.php
@@ -1,2 +1,42 @@
-<?php 
+<?php
+
+use sbwms\Employee\Employee;
+use sbwms\Employee\EmployeeMapper;
+use sbwms\Employee\EmployeeRepository;
+
+if ($request->getMethod() === 'POST') {
+
+    /** @var array */
+    $formData = $request->request->getIterator()->getArrayCopy();
+
+    /** @var EmployeeMapper */
+    $employeeMapper = new EmployeeMapper($pdoAdapter);
+
+    /** @var Employee */
+    $employee = $employeeMapper->create($formData);
+
+    /** @var EmployeeRepository */
+    $employeeRepository = new EmployeeRepository($employeeMapper);
+
+    /** @var true|null */
+    $result = $employeeRepository->save($employee);
+    
+    /* 
+        0 = success
+        1 = failure (expect error list)
+    */
+
+    /** @var int */
+    $successStatus = 1;
+
+    if ($result === true) { $successStatus = 0; }
+    $resultArray = [
+        'result' => $result,
+        'success' => $successStatus,
+    ];
+
+    echo json_encode($resultArray); // buffered
+    return;
+}
+
 require_once VIEWS . 'employee/createEmployee.view.php';
