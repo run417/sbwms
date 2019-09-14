bae76d2ec1358fc464b1f4aa57a5d9ae862da540
Vinura <vinuraeu@gmail.com>
2019-09-14 22:50:51 +0530

    Add Booking class

---
+++ src/model/Booking/Booking.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace sbwms\Booking;
+
+class Booking {
+    /** @var string */
+    private $bookingId;
+
+    /**
+     * Get the value of bookingId
+     */ 
+    public function getBookingId()
+    {
+        return $this->bookingId;
+    }
+}
