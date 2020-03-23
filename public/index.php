5369f1bf91d45b1799565cb75ca26347b0ec6309
Vinura <vinuraeu@gmail.com>
2020-03-23 19:31:29 +0530

    Comment old code from merges
    Don't won't to delete right now

--- public/index.php
+++ public/index.php
@@ -68,42 +68,42 @@ if (!isset($response) || !($response instanceof Response)) {
 $response->prepare($request);
 $response->headers->set('X-Sent-By', 'symfony http-foundation');
 
-$path = formatPath($request->getPathInfo());
+// $path = formatPath($request->getPathInfo());
 
-$map = [
-    '/login' => 'login',
-    '/' => 'dashboard',
-    '/dashboard' => 'dashboard',
-    '/booking' => 'booking/list',
-    '/booking/new' => 'booking/new',
-    '/customer' => 'customer/list',
-    '/customer/new' => 'customer/new',
-    '/customer/edit' => 'customer/edit',
-    '/customer/view' => 'customer/view',
-    '/employee' => 'employee/list',
-    '/employee/new' => 'employee/new',
-    '/employee/edit' => 'employee/edit',
-    '/employee/view' => 'employee/view',
-    '/system' => 'system/user/list',
-    '/system/user' => 'system/user/list',
-    '/system/user/new' => 'system/user/new',
+// $map = [
+//     '/login' => 'login',
+//     '/' => 'dashboard',
+//     '/dashboard' => 'dashboard',
+//     '/booking' => 'booking/list',
+//     '/booking/new' => 'booking/new',
+//     '/customer' => 'customer/list',
+//     '/customer/new' => 'customer/new',
+//     '/customer/edit' => 'customer/edit',
+//     '/customer/view' => 'customer/view',
+//     '/employee' => 'employee/list',
+//     '/employee/new' => 'employee/new',
+//     '/employee/edit' => 'employee/edit',
+//     '/employee/view' => 'employee/view',
+//     '/system' => 'system/user/list',
+//     '/system/user' => 'system/user/list',
+//     '/system/user/new' => 'system/user/new',
 
-    '/service/type' => 'service/type/list',
-    '/service/type/new' => 'service/type/new',
-    '/service/type/new' => 'service/type/edit',
+//     '/service/type' => 'service/type/list',
+//     '/service/type/new' => 'service/type/new',
+//     '/service/type/new' => 'service/type/edit',
 
-    '/test' => 'test/test.database'
-];
+//     '/test' => 'test/test.database'
+// ];
 
-if (isset($map[$path])) {
-    ob_start();
-    include_once sprintf(projectRoot().'/src/handlers/%s.php', $map[$path]);
-    $response->setContent(ob_get_clean());
-} else {
-    $response->setStatusCode(404);
-    ob_start();
-    include_once projectRoot() . '/src/handlers/404.php';
-    $response->setContent(ob_get_clean());
-}
+// if (isset($map[$path])) {
+//     ob_start();
+//     include_once sprintf(projectRoot().'/src/handlers/%s.php', $map[$path]);
+//     $response->setContent(ob_get_clean());
+// } else {
+//     $response->setStatusCode(404);
+//     ob_start();
+//     include_once projectRoot() . '/src/handlers/404.php';
+//     $response->setContent(ob_get_clean());
+// }
 
 $response->send();
